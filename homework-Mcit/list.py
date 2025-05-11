c1231b78ad1447adae91a150276d406c2412be2d
nicolas delisle <nicolas.delisle@hotmail.ca>
2025-05-11 15:57:12 -0400

    commit

--- homework-Mcit/list.py
+++ homework-Mcit/list.py
@@ -1,25 +1,25 @@
-#prompt the user to get 5 number one at time
-numbers = []   #Float or Integer
-for i in range(5):
+#prompt the user to get 5 number one at time 
+numbers = []                                                             #initializes a empty list
+for i in range(5):                                                       # start a loop for 5 number
+    #making sure to receive number no other character
     while True:
         try:
             number = float(input("Enter a number: "))
-            if number.is_integer:    #making integer look cleaner
+            if number.is_integer():                                      #making integer look cleaner
              number = int(number)
-            numbers.append(number)
-            break
-        except ValueError:
-            print('Invalid input! Try enter a number.')
+            numbers.append(number)                                       #add a number to the numbers 
+            break                                                        
+        except ValueError:                                               #if not a valid anwser prompt the user again
+            print('Invalid input! Try enter a number.')                  
 
-
-
-
-#sorting number ascend and remove the highest number   
+#sorting number ascend    
 numbers.sort()
 #average of all numbers
-average_all = sum(numbers) / len(numbers)         #need to be placed before the number pop
-numbers.pop()
+average_all = sum(numbers) / len(numbers) 
+print("You entered:", numbers)                                           #same as line 27 but since list is dynamic it show numbers before removing the highest and the average
 
+#removing Highest
+numbers.pop()
 #average of remaining numbers
 average_remain = sum(numbers) / len(numbers)
    
