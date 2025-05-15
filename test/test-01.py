f49e5afe872d9d1d4dbcb864e4f8febef2fb1987
nicolas delisle <nicolas.delisle@hotmail.ca>
2025-05-14 20:13:48 -0400

    commit

--- test/test-01.py
+++ test/test-01.py
@@ -1,6 +1,3 @@
-numbers = [1, 1, 2, 3, 4, 5]
-first = set(numbers)
-second = {1, 8}
-
-print(first | second)
-print(first ^ second)
+string1 = "this is montreal"
+len1 = len(string1)
+print("the length of the string is:", len1)
