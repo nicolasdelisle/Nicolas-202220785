bc5f69b55c452bdde466bf1b6238f2524ca9d2a6
nicolas delisle <nicolas.delisle@hotmail.ca>
2025-05-16 22:00:12 -0400

    commit

--- homework_06/01-regex-homeweork.py
+++ homework_06/01-regex-homeweork.py
@@ -2,7 +2,7 @@ import re
 
 text = "Please contact us at support@example.com or sales@company.org. You can also reach out to admin123@school.edu."
 
-# Simple regex that matches any text with '@'  \S+ needed both side for full email adress
+# regex that matches any text with '@'  \S+ needed both side for all character until a space each side 
 email_add = r'\S+@\S+'
 
 email = re.findall(email_add, text)
