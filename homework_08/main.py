57a18101d1d65b133f3734ede4f3cef55d477a99
nicolas delisle <nicolas.delisle@hotmail.ca>
2025-05-22 17:35:56 -0400

    commit

---
+++ homework_08/main.py
@@ -0,0 +1,17 @@
+import temperature
+def main():
+    try:
+        celsius = float(input("Enter temperature in Celsius: "))
+        fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+
+        converted_to_fahrenheit = temperature.celsius_to_fahrenheit(celsius)
+        converted_to_celsius = temperature.fahrenheit_to_celsius(fahrenheit)
+
+        print(f"{celsius:.2f}°C is {converted_to_fahrenheit:.2f}°F")
+        print(f"{fahrenheit:.2f}°F is {converted_to_celsius:.2f}°C")
+        
+    except ValueError:
+        print("Please enter valid numeric values.")
+
+
+main()
---
+++ homework_08/temperature.py
@@ -0,0 +1,7 @@
+def celsius_to_fahrenheit(celsius):
+    """Convert Celsius to Fahrenheit."""
+    return celsius * (9/5) + 32
+
+def fahrenheit_to_celsius(fahrenheit):
+    """Convert Fahrenheit to Celsius."""
+    return (fahrenheit - 32) * (5/9)
