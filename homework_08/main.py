32dcdf178c97d1890fac1fb1d3bd6ac59144b989
nicolas delisle <nicolas.delisle@hotmail.ca>
2025-05-23 20:35:09 -0400

    commit

--- homework_08/main.py
+++ homework_08/main.py
@@ -7,8 +7,8 @@ def main():
         converted_to_fahrenheit = temperature.celsius_to_fahrenheit(celsius)
         converted_to_celsius = temperature.fahrenheit_to_celsius(fahrenheit)
 
-        print(f"{celsius:.2f}°C is {converted_to_fahrenheit:.2f}°F")
-        print(f"{fahrenheit:.2f}°F is {converted_to_celsius:.2f}°C")
+        print(f"{celsius}°C is {converted_to_fahrenheit:.2f}°F")
+        print(f"{fahrenheit}°F is {converted_to_celsius:.2f}°C")
         
     except ValueError:
         print("Please enter valid numeric values.")
